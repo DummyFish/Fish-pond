475771df41c70b57b1bec357f0f5bb11d00c9d7c
Damu Gao <jasongao321@gmail.com>
2020-05-11 21:47:32 -0700

    telnet

--- Honeypot/services/telnet/telnet.py
+++ Honeypot/services/telnet/telnet.py
@@ -1,38 +1,74 @@
 import sys
 import logging
-import telnetlib
-import time
-import argparse
-from socket import socket, AF_INET, SOCK_STREAM
+import threading
+import signal
+from services.origin_service import Service
+import os
+import configparser
 
-VERSION = '1'
-welcome = b"login: "
+sys.path.append('..')
 
-def send_email(src_address):
-    pass
+def signal_handler(signal, frame):
+    print("\nClosing out cleanly...")
+    telnet_server.SERVER_RUN = False
 
-def telnet(address,port=23):
+def define_logger(settings):
+    FORMAT = '%(asctime)s - %(name)s - %(message)s'
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG,
+                        format=FORMAT)
+
     try:
-        ski=socket(AF_INET,SOCK_STREAM)
-        ski.bind((address, port))
-        ski.listen()
-        conn,addr = ski.accept()
-        print('visited' + addr[0])
-        send_email(addr[0])
-        conn.sendall(welcome)
-        while True:
-            data=conn.recv(1024)
-            if data == b'\r\n':
-                ski.close()
-                sys.exit()
-    except: 
-        ski.close()
-        sys.exit()
-    
+        infohandler = logging.FileHandler(settings['log_location'])
+    except:
+        infohandler = logging.FileHandler("telnet-log.txt")
+    infohandler.setLevel(logging.INFO)
+    infohandler.setFormatter(logging.Formatter(FORMAT))
+
+    debughandler = logging.StreamHandler()
+    debughandler.setLevel(logging.DEBUG)
+
+    logger = logging.getLogger(settings['hostname'])
+    logger.addHandler(infohandler)
+
+    logging.getLogger("requests").setLevel(logging.WARNING)
+
+
+def parse_config():
+
+    settings = {}
+    config = configparser.ConfigParser()
+    config.read('telnet.cfg')
+    settings['port'] = config.getint('Telnet', 'port')
+    settings['image'] = config.get('Telnet', 'image')
+    settings['passwordmode'] = config.getboolean('Telnet', 'password-mode')
+    settings['hostname'] = config.get('Telnet', 'hostname')
+    settings['log_location'] = config.get('Telnet', 'log')
+    settings['address'] = config.get('Telnet', 'address')
+
+    return settings
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='honeypot telnet',
-                                 epilog='Version: ' + str(VERSION))
-    parser.add_argument('-a','--address',help='server ip address to use',action='store', required=True)   
-    args = parser.parse_args()
-    
-    telnet(args.address)
+
+    signal.signal(signal.SIGINT, signal_handler)
+
+    settings = parse_config()
+
+    define_logger(settings)
+
+    telnet_server = Service(
+        hostname = settings['hostname'],
+        port = settings['port'],
+        address = settings['address'],
+        image = settings['image'],
+        passwordmode = settings['passwordmode'],
+    )
+
+    logger = logging.getLogger(settings['hostname'])
+    logger.info("[SERVER] Listening for connections on port {}. CTRL-C to break.".
+                format(telnet_server.port))
+    while telnet_server.SERVER_RUN is True:
+        telnet_server.poll()
+        telnet_server.kick_idle()
+        telnet_server.process_clients()
+
+    telnet_server.clean_exit()
