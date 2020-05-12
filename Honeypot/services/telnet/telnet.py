0ef468eff82f8d7a24e4de5bd2d6a2d2363c08fd
Damu Gao <jasongao321@gmail.com>
2020-05-11 21:00:59 -0700

    telnet fix

--- Honeypot/services/telnet/telnet.py
+++ Honeypot/services/telnet/telnet.py
@@ -3,45 +3,40 @@ sys.path.append('..')
 import logging
 import telnetlib
 import time
+import sys
+import argparse
+from socket import socket, AF_INET, SOCK_STREAM
 
+VERSION = '1'
+welcome = b"login: "
 
-class TelnetClient():
-    def __init__(self,):
-        self.tn = telnetlib.Telnet()
+def send_email(src_address):
+    pass
 
-    def login_host(self,host_ip,username,password):
-        try:
-            self.tn.open(host_ip,port=23)
-        except:
-            logging.warning('%sfail'%host_ip)
-            return False
-
-        self.tn.read_until(b'login: ',timeout=20)
-        self.tn.write(username.encode('ascii') + b'\n')
-
-        self.tn.read_until(b'Password: ',timeout=20)
-        self.tn.write(password.encode('ascii') + b'\n')
-
-        time.sleep(2)
-        
-        command_result = self.tn.read_very_eager().decode('ascii')
-        if 'Login incorrect' not in command_result:
-            logging.warning('%ssuccess'%host_ip)
-            return True
-        else:
-            logging.warning('%sfail'%host_ip)
-            return False
-
-    def execute_some_command(self,command):
-      
-        self.tn.write(command.encode('ascii')+b'\n')
-        time.sleep(2)
+def telnet(address,port=23):
+    try:
+        ski=socket(AF_INET,SOCK_STREAM)
+        ski.bind((address, port))
+        ski.listen()
+        conn,addr = ski.accept()
+        print('visited' + addr[0])
+        send_email(addr[0])
+        conn.sendall(welcome)
+        while True:
+            data=conn.recv(1024)
+            if data == b'\r\n':
+                ski.close()
+                sys.exit()
+    except: 
+        ski.close()
+        sys.exit()
     
-        command_result = self.tn.read_very_eager().decode('ascii')
-        logging.warning('result：\n%s' % command_result)
-
-
-    def logout_host(self):
-        self.tn.write(b"exit\n")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='honeypot telnet',
+                                 epilog='Version: ' + str(VERSION))
+    parser.add_argument('-a','--address',help='server ip address to use',action='store', required=True)   
+    args = parser.parse_args()
+    
+    telnet(args.address)
 
 
